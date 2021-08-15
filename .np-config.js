aff2a8ca7dae46b3125bab7aac073f111c28ed03
palerdot <palerdot@gmail.com>
2021-08-15 09:36:24 +0530

    chore: turn on yarn back in np

--- .np-config.js
+++ .np-config.js
@@ -1,4 +1,6 @@
+// IMPORTANT:
+// `npm run release` for publishing so that
+// we don't run into - https://github.com/sindresorhus/np/issues/507
 module.exports = {
-  yarn: false,
   contents: 'dist',
 }
